9e4b3281be7a9f173ef19720b0453499d7a140fb
dtrupenn <dtrupenn@gmail.com>
2013-03-04 16:48:13 -0500

    Initial setup done

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HealthApp::Application.config.secret_token = '1cd66bf0ff9b214f1ccb2e953f690258f88d66409962133545c14910b445b596ebf3c264dad56c93e0ea6598352d3f85a2e7e1335a00f7a4ddacca1351f42dc1'
