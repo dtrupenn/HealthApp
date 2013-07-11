168e8091b960fc4fc9c079804369bcd61213fbd3
dtrupenn <dtrupenn@gmail.com>
2013-07-11 16:48:15 -0400

    Fixing heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+  # protect_from_forgery
   
   # Handle CanCan::AccessDenied Exception
   rescue_from CanCan::AccessDenied do |exception|
